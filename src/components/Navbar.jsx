d709f70fd435db87d04a83d8acadab6f29f55f35
ishitadudhat <ishitadudhat2501@gmail.com>
2024-11-19 20:20:47 +0530

    Add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-dark text-white navbar-expand-lg">
+      <div className="container">
+        <a className="navbar-brand" href="/">E-Shopper</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item"><a className="nav-link" href="#home">Home</a></li>
+            <li className="nav-item"><a className="nav-link" href="#shop">Shop</a></li>
+            <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
+            <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
